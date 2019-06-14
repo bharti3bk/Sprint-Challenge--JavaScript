c21bd1bd3cc17a5aed148a43232d2285456b58f3
bharti3bk <bharti3bk@gmail.com>
2019-06-14 14:31:53 -0400

    completed task3

--- challenges/functions.js
+++ challenges/functions.js
@@ -10,7 +10,6 @@ function consume(a,b,cb) {
   return cb(a,b);
 }
 
-
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
